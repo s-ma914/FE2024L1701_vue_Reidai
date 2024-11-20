77092c2cc8fb16d80778336bb79b19909de48b3f
s-ma914 <2301376@s.asojuku.ac.jp>
2024-11-20 11:17:49 +0900

    reidai

--- Reidai/script/script.js
+++ Reidai/script/script.js
@@ -1,16 +1,17 @@
-// JavaScript用ファイル
 new Vue({
-    el: '#app',
-    
-    data: {
-    },
-    computed: {
-      // 入力されている、且つパスワードが一致しているかを確認する算出プロパティ
-      // 入力されているかは、this.･･･ で判定可能
-      // 例）一致TRUE（パスワード：aaaa、パスワード確認：aaaa）
-      // 例）不一致FALSE（パスワード：aaaa、パスワード確認：aaab）
-
-      // ボタンをTRUEなら登録を表示orFALSEなら使用不可を表示にする算出プロパティ
-
-    }
-  });
+  el: '#app',
+  data: {
+      password: '',      // パスワード入力
+      password2: '' // パスワード確認入力
+  },
+  computed: {
+      
+      passmatched() {
+          return this.password !=='' && this.password === this.password2;
+      },
+      
+      button() {
+          return this.passmatched;
+      }
+  }
+});
